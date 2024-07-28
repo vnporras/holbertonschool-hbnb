1f626dee08f86185386dc5f20117d97471850204
vnporras <vnporras1028@gmail.com>
2024-07-28 18:51:59 -0500

    add the project

--- hbnb.py
+++
@@ -1,8 +0,0 @@
-""" Another way to run the app"""
-
-from src import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run()
--- utils/constants.py
+++
@@ -1,6 +0,0 @@
-""" Export constants for the application """
-
-REPOSITORY_ENV_VAR = "REPOSITORY"
-
-FILE_STORAGE_FILENAME = "data.json"
-PICKLE_STORAGE_FILENAME = "data.pkl"
