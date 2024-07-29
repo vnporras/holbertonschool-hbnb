72532edf9b216e408095c3cd238e00222ff3ebde
Vanessa Porras <79481309+vnporras@users.noreply.github.com>
2024-07-28 19:02:40 -0500

    Delete hbnb directory

--- hbnb/hbnb.py
+++
@@ -1,9 +0,0 @@
-""" Another way to run the app"""
-
-from src import create_app
-from src.config import ProductionConfig
-
-app = create_app(ProductionConfig)
-
-if __name__ == "__main__":
-    app.run()
